88cba896f51d36052b94c0f53b4ed35e032cc203
Giulia <giuliapupo0@gmail.com>
2020-11-03 09:54:27 +0100

    Prima versione del convertitore da decimale a binario

---
+++ Convertitre 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Convertitre_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerodecimale;
+            int resto;
+            string numeroconvertito = "";
+            int divisore;
+
+            Console.WriteLine("Inserisci un numero:");
+            numerodecimale = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserisci un divisore");
+            divisore = Convert.ToInt32(Console.ReadLine());
+
+            while (numerodecimale > 0)
+            {
+                resto = numerodecimale % divisore;
+                numerodecimale = numerodecimale / 2;
+                numeroconvertito = Convert.ToString(resto) + numeroconvertito;
+            }
+            Console.WriteLine($"{numeroconvertito}");
+            Console.ReadLine();
+        }
+    }
+}
