e751f6dc8baa03e338f9ccf6b6857b4816fe3527
Giulia <giuliapupo0@gmail.com>
2020-11-03 11:10:04 +0100

    Convertitore decimale-binario

--- Convertitre 2/Program.cs
+++ Convertitre 2/Program.cs
@@ -13,14 +13,44 @@ namespace Convertitre_2
 
             Console.WriteLine("Inserisci un numero:");
             numerodecimale = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
             Console.WriteLine("Inserisci un divisore");
             divisore = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
 
             while (numerodecimale > 0)
             {
                 resto = numerodecimale % divisore;
-                numerodecimale = numerodecimale / 2;
-                numeroconvertito = Convert.ToString(resto) + numeroconvertito;
+                numerodecimale = numerodecimale / divisore;
+
+                if (resto == 10)
+                {
+                    numeroconvertito = "A" + numeroconvertito;
+                }
+                else if (resto == 11)
+                {
+                    numeroconvertito = "B" + numeroconvertito;
+                }
+                else if (resto == 12)
+                {
+                    numeroconvertito = "C" + numeroconvertito;
+                }
+                else if (resto == 13)
+                {
+                    numeroconvertito = "D" + numeroconvertito;
+                }
+                else if (resto == 14)
+                {
+                    numeroconvertito = "E" + numeroconvertito;
+                }
+                else if (resto == 15)
+                {
+                    numeroconvertito = "F" + numeroconvertito;
+                }
+                else
+                {
+                    numeroconvertito = Convert.ToString(resto) + numeroconvertito;
+                }
             }
             Console.WriteLine($"{numeroconvertito}");
             Console.ReadLine();
